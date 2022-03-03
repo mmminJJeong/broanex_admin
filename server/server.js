6b8ff494dccfbecea4a5e12ed2eaaa4cb7a36fde
mmminJJeong <flzn0100@gmail.com>
2022-03-04 01:37:20 +0900

    서버연결

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const path = require("path");
+const cors = require("cors");
 const app = express();
 
-const PORT = process.env.PORT || 4000;
+const server = require("http").createServer(app);
+
+app.use(cors()); // cors 미들웨어를 삽입합니다.
 
 app.get("/", (req, res) => {
-  res.send(`Response Complate`);
+  // 요청패스에 대한 콜백함수를 넣어줍니다.
+  res.send({ message: "hello" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server On : http://localhost:${PORT}/`);
+server.listen(8080, () => {
+  console.log("server is running on 8080");
 });
--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import "./App.css";
-import Router from "./router/router";
-import Header from "./inc/header";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 function App() {
-  return (
-    <>
-      <>
-        <Header />
-        <Router></Router>
-      </>
-    </>
-  );
+  const sendRequest = async () => {
+    const response = await axios.get("http://localhost:8080");
+    console.log(response);
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    sendRequest();
+  });
+
+  return <div className="App"></div>;
 }
 
 export default App;
