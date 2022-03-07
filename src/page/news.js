851ed07f670eb9d0c00c1371a53162a5c977ed4c
mmminJJeong <flzn0100@gmail.com>
2022-03-07 10:23:45 +0900

    복구 완료

--- src/page/news.js
+++ src/page/news.js
@@ -1,14 +1,82 @@
-import React from "react";
-
-import MainSection from "../component/layout/main";
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import parse from "html-react-parser";
+import "./page.css";
 
 export default function NewsEditor() {
+  const [newscontent, setNewsContent] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [viewContent, setViewContent] = useState([]);
+
+  const getValue = e => {
+    const { name, value } = e.target;
+    setNewsContent({
+      ...newscontent,
+      [name]: value,
+    });
+    console.log(newscontent);
+  };
+
   return (
-    <MainSection>
+    <div className="main-wrapper">
+      <div className="news-title">
+        <h2>게시물</h2>
+      </div>
+      <div className="news-container">
+        {viewContent.map(Element => (
+          <div>
+            <h2>{Element.title}</h2>
+            <div>{parse(Element.content)}</div>
+          </div>
+        ))}
+      </div>
       <div className="news-title">
         <h2>뉴스 게시글 작성</h2>
       </div>
-      <div></div>
-    </MainSection>
+      <div className="editor-inside">
+        <input
+          className="input-title"
+          type="text"
+          placeholder="제목을 입력해주세요."
+          onChange={getValue}
+          name="title"
+        />
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>내용을 입력해주세요.</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log("Editor is ready to use!", editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+            setNewsContent({
+              ...newscontent,
+              content: data,
+            });
+            console.log(newscontent);
+          }}
+          onBlur={(event, editor) => {
+            console.log("Blur.", editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log("Focus.", editor);
+          }}
+        />
+        <button
+          className="submit-button"
+          onClick={() => {
+            setViewContent(viewContent.concat({ ...newscontent }));
+          }}
+        >
+          입력
+        </button>
+      </div>
+    </div>
   );
 }
