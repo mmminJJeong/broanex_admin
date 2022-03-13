a6b6905329d397c4a8f30ee81ce7176d9c161e7f
mmminJJeong <flzn0100@gmail.com>
2022-03-13 22:45:42 +0900

    리셋 후 재 창조

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
-import "./App.css";
-import "./reset.css";
-import MainHeader from "./component/layout/header";
-import SideMenu from "./component/sideMenu/sidemenu";
-import Router from "./pageRouter/pagerouter";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <div className="wrap">
-        <MainHeader />
-        <SideMenu />
-        <Router />
-      </div>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- server/Router/boardNotice.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET home page. */
-router.get("/notice", function (req, res, next) {
-  res.send("noticeIndex");
-});
-
-module.exports = router;
